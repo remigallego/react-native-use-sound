b4b3e438913e1e6ef7342c4cb28ccddd7ea3d9d0
Remi <remi.gallego@hybridheroes.de>
2020-09-14 11:50:36 +0200

    refactor: validateUrl

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,4 @@
 export const validURL = (str: string) => {
-  try {
-    new URL(str);
-  } catch (_) {
-    return false;
-  }
-
-  return true;
+  var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+  return !!urlregex.test(str);
 };
